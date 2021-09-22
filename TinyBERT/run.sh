c54d3cc6df0d88f1f67bb3acad7b92a20f50e92c
Kan Wu (FA Talent) <v-kanwu@microsoft.com>
2021-09-22 20:21:40 +0800

    add run.sh

--- TinyBERT/run.sh
+++ TinyBERT/run.sh
@@ -5,7 +5,7 @@ BERT_BASE_DIR="/mnt/configblob/users/v-jinnian/bert/trained_models"
 CORPUS_JSON_DIR="/mnt/configblob/users/v-jinnian/bert/processed_data"
 
 python pregenerate_training_data.py --train_corpus $CORPUS_RAW \ 
-                  --bert_model $BERT_BASE_DIR$ \
+                  --bert_model $BERT_BASE_DIR \
                   --reduce_memory --do_lower_case \
                   --epochs_to_generate 3 \
-                  --output_dir $CORPUS_JSON_DIR$ 
+                  --output_dir $CORPUS_JSON_DIR
