c5e44c0ccac863d6e90b358a1281b2d5d09e1f72
Kan Wu (FA Talent) <v-kanwu@microsoft.com>
2021-09-22 20:34:29 +0800

    add run.sh

--- TinyBERT/run.sh
+++ TinyBERT/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-CORPUS_RAW="/mnt/configblob/users/v-jinnian/wiki/text"
+#CORPUS_RAW="/mnt/configblob/users/v-jinnian/wiki/text"
+CORPUS_RAW="/mnt/configblob/users/v-jinnian/wiki/text/AA/wiki_00"
 BERT_BASE_DIR="/mnt/configblob/users/v-jinnian/bert/trained_models/uncased_L-12_H-768_A-12"
 CORPUS_JSON_DIR="/mnt/configblob/users/v-jinnian/bert/processed_data"
 
